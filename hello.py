08f1191081f9f154033be49e23020fa57e527361
Jazim <jazim.a.29@gmail.com>
2023-09-20 10:55:09 -0400

    Add new template to render name and timestamp

--- hello.py
+++ hello.py
@@ -1,13 +1,20 @@
+from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template("base.html")
+    return render_template('user.html', name="Jazim", current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
